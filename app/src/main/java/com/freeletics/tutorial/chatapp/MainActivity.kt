e081c6e4305dbb05f9798fee0397323c0f761e21
Hannes Dorfmann <hannes.dorfmann@gmail.com>
2019-03-19 11:17:49 +0100

    Initial commit

---
+++ app/src/main/java/com/freeletics/tutorial/chatapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.freeletics.tutorial.chatapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        send.setOnClickListener {
+
+        }
+    }
+}
